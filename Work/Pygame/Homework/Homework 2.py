6c4b424f4b0af9f2b442bad5fc84400465f8d747
JakeThePygrammer <jakovnestorovski@gmail.com>
2025-01-31 22:20:11 +0100

    Added new scripts and images

--- Work/Pygame/Homework/Homework 2.py
+++ Work/Pygame/Homework/Homework 2.py
@@ -2,6 +2,7 @@ import pygame as pg
 import pygamebg
 text = "Tomb Adventure"
 text2 = "Start level"
+text3 = "Quit"
 (width,height) = (960,540)
 window = pygamebg.open_window(width,height,"Name and surname")
 window.fill((0,0,0))
@@ -12,16 +13,36 @@ Money = pg.image.load("../Images/Money.png")
 PaperScroll = pg.image.load("../Images/PaperScroll.png")
 font = pg.font.SysFont("Papyrus", 50, True, False)
 textadded = font.render(text, True, pg.Color("Black"))
-font = pg.font.SysFont("Papyrus", 50, True, False)
+font = pg.font.SysFont("Papyrus", 50, False, False)
 textadded2 = font.render(text2, True, pg.Color("Black"))
+textadded3 = font.render(text3, True, pg.Color("Black"))
 wt, ht = textadded.get_width(), textadded.get_height()
 wt1, ht1 = textadded2.get_width(), textadded2.get_height()
+wt2 = textadded2.get_width()
 Background = pg.transform.scale(Background, (960, 540))
+Papyrus = pg.transform.rotate(PaperScroll, 270)
 window.blit(Background, (0, 0))
 x = (width - wt) // 2
 y = (height // 2) - 50
 x1 = (width - wt1) // 2
 y1 = (height // 2)
-window.blit(textadded, (x,y - 20))
+x2 = 417
+
+Papyrus = pg.transform.scale(Papyrus, (460, 95))
+window.blit(Papyrus, (x - 40, y-30))
+Papyrus = pg.transform.scale(Papyrus, (300, 95))
+window.blit(Papyrus, (x1 - 40, y1 - 10))
+Papyrus = pg.transform.scale(Papyrus, (300, 95))
+window.blit(Papyrus, (x1 - 40, y1 + 70))
+Protagonist = pg.transform.scale(Protagonist, (250, 300))
+window.blit(Protagonist, (600,250))
+Mummy = pg.transform.scale(Mummy, (200, 250))
+window.blit(Mummy, (50,300))
+Coins = pg.transform.scale(Money, (100, 100))
+window.blit(Coins, (300,440))
+
+
 window.blit(textadded2, (x1,y1))
+window.blit(textadded, (x,y - 20))
+window.blit(textadded3, (x2,y1 + 80))
 pygamebg.wait_loop()

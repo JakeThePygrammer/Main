a38a3b7321dbaa7850b9295fc6bf8150d33bc0f0
JakeThePygrammer <jakovnestorovski@gmail.com>
2025-02-22 15:32:00 +0100

    Added new scripts and images

--- Work/Pygame/Exercises/Exercise 28.py
+++ Work/Pygame/Exercises/Exercise 28.py
@@ -1,41 +1,38 @@
 import pygame as pg
 import time
 
-# Иницијализација на Pygame
 pg.init()
-
-# Поставување на екран
 SIRINA, VISINA = 800, 600
 ekran = pg.display.set_mode((SIRINA, VISINA))
-pg.display.set_caption("Контрола на  квадрат")
+pg.display.set_caption("Контрола на квадрат")
 
-# Почетна позиција, големина и боја на квадратот
 kvadrat = pg.Rect(375, 275, 50, 50)
-boja = (0, 0, 255)  # Почетна боја: сина
+boja = (0, 0, 255)
 
-# Брзина на движење
 brzina = 2
 skok_sila = 15
 skoka = False
 
-# Време на последна промена на боја
+sizedown = pg.mixer.Sound("../Audio/mixkit-arrow-whoosh-1491.wav")
+sizeup = pg.mixer.Sound("../Audio/mixkit-retro-game-notification-212.wav")
+
 posledna_promena = time.time()
+color_change_time = 0
 square_x, square_y = 200, 200
 square_size = 60
-# Главна програма
+Color = "Blue"
+
 running = True
 while running:
-    ekran.fill((255, 255, 255))  # Позадина
+    ekran.fill((255, 255, 255))
 
     for nastan in pg.event.get():
         if nastan.type == pg.QUIT:
             running = False
-
         elif nastan.type == pg.KEYDOWN:
             if nastan.key == pg.K_ESCAPE:
                 running = False
 
-    # Движење со WASD
     keys = pg.key.get_pressed()
     if keys[pg.K_a]:
         square_x -= brzina
@@ -47,12 +44,22 @@ while running:
         square_y += brzina
     if keys[pg.K_PLUS] or keys[pg.K_EQUALS]:
         square_size += 10
+        sizeup.play()
+        Color = "Green"
+        color_change_time = time.time()
     if keys[pg.K_MINUS] or keys[pg.K_UNDERSCORE]:
         square_size -= 10
+        sizedown.play()
+        Color = "Red"
+        color_change_time = time.time()
+
+    if time.time() - color_change_time > 3:
+        Color = "Blue"
+
     square_x = max(0, min(SIRINA - square_size, square_x))
     square_y = max(0, min(VISINA - square_size, square_y))
-    # Цртање на квадратот
-    pg.draw.rect(ekran,(pg.Color("Blue")), (square_x, square_y, square_size, square_size))
+
+    pg.draw.rect(ekran, pg.Color(Color), (square_x, square_y, square_size, square_size))
     pg.display.flip()
     pg.time.delay(5)
 

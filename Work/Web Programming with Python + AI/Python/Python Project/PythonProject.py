bfd827f0be7ce5779f5d1f1c8a15b560e8747222
JakethePygrammer <jakovnestorovski@gmail.com>
2026-02-07 19:00:31 +0100

    Project work

--- Work/Web Programming with Python + AI/Python/Python Project/PythonProject.py
+++ Work/Web Programming with Python + AI/Python/Python Project/PythonProject.py
@@ -1,9 +1,11 @@
 import requests
+import random
 
 print("Option 1 - country information")
 print("Option 2 - country comparison")
 print("Option 3 - quiz")
 print("Option 4 - search history")
+print("Option 5 - other")
 options = input("Choose one(enter the corresponding number) : ")
 
 if options == "1":
@@ -20,6 +22,9 @@ if options == "1":
         a = list(response1[0]["currencies"].keys())
         print(f"Currency : {a[0]} - {response1[0]["currencies"][a[0]]["name"]}")
         print(f"Flag : {response1[0]["flags"]["png"]}")
+        file = open("SearchText.txt", "a")
+        file.write(f"{response1[0]["name"]["common"]} - {response1[0]["capital"][0]} - {response1[0]["continents"][0]}\n")
+        file.close()
 
 if options == "2":
     name1 = input("Enter country 1 : ")
@@ -39,3 +44,36 @@ if options == "2":
             print("Both countries have an equal population.")
 
 if options == 3:
+    list1 = []
+    list2 = []
+    responsec1 = requests.get(f"https://restcountries.com/v3.1/all?fields=name,capital")
+    responsecountry1 = responsec1.json()
+    print(responsecountry1)
+    for item in responsecountry1:
+        list1.append(item["name"]["common"])
+        list2.append(item["capital"])
+
+    counter = 0
+    for index1 in range(5):
+        guess = random.choice(list1)
+        b = list2[list1.index(guess)][0]
+        print(b)
+        for index in range(3):
+            a = input(f"What is the capital of {guess} : ")
+            if a.lower() == b.lower():
+                print(f"Correct! The capital of {guess} is {b}!")
+                counter += 1
+                break
+
+    print(f"Out of 5 countries capitals, you got {counter} right!")
+
+if options == "4":
+    file1 = open("SearchText.txt", "r")
+    filelist = file1.readlines()
+    print("\nYour search history : \n")
+    for line in filelist:
+        print(line, end="")
+    file1.close()
+
+if options == "5":
+    print("TBD")
